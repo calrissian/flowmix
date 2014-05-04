a37100c9583e41d5ff7f2a83f5b664768305f981
Corey J. Nolet <cjnolet@gmail.com>
2014-05-03 21:16:33 -0400

    Formatting change of the examples to make it prettier

--- src/main/java/org/calrissian/flowbox/example/StreamExportExample.java
+++ src/main/java/org/calrissian/flowbox/example/StreamExportExample.java
@@ -17,54 +17,54 @@ public class StreamExportExample implements FlowProvider {
   @Override
   public List<Flow> getFlows() {
     Flow flow = new FlowBuilder()
-            .id("myFlowId")
-            .flowDefs()
-            .stream("stream1")
-            .filter().criteria(new Criteria() {
+      .id("myFlowId")
+      .flowDefs()
+        .stream("stream1")
+          .filter().criteria(new Criteria() {
               @Override
               public boolean matches(Event event) {
                 return true;
               }
             }).end()
-            .select().field("key3").end()
-            .partition().field("key3").end()
-            .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
-            .endStream()
-            .endDefs()
-            .createFlow();
+          .select().field("key3").end()
+          .partition().field("key3").end()
+          .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
+        .endStream()
+      .endDefs()
+    .createFlow();
 
     Flow flow2 = new FlowBuilder()
-            .id("myFlowId2")
-            .flowDefs()
-            .stream("stream1")
-            .filter().criteria(new Criteria() {
+      .id("myFlowId2")
+      .flowDefs()
+        .stream("stream1")
+          .filter().criteria(new Criteria() {
               @Override
               public boolean matches(Event event) {
                 return true;
               }
             }).end()
-            .partition().field("key5").end()
-            .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
-            .endStream(false, "stream2")   // send ALL results to stream2
-            .stream("stream2")
-            .filter().criteria(new Criteria() {
+          .partition().field("key5").end()
+          .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
+        .endStream(false, "stream2")   // send ALL results to stream2
+        .stream("stream2")
+          .filter().criteria(new Criteria() {
               @Override
               public boolean matches(Event event) {
                 return true;
               }
             }).end()
-            .select().field("key4").end()
-            .partition().field("key4").end()
-            .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
-            .each().function(new Function() {
-              @Override
-              public List<Event> execute(Event event) {
-                return singletonList(event);
-              }
-            }).end()
-            .endStream()
-            .endDefs()
-            .createFlow();
+          .select().field("key4").end()
+          .partition().field("key4").end()
+          .stopGate().activate(Policy.TIME_DELTA_LT, 1000).evict(Policy.COUNT, 5).open(Policy.TIME, 5).end()
+          .each().function(new Function() {
+            @Override
+            public List<Event> execute(Event event) {
+              return singletonList(event);
+            }
+          }).end()
+        .endStream()
+      .endDefs()
+    .createFlow();
 
     return Arrays.asList(new Flow[] { flow, flow2});
   }

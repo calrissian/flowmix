77109699441312a739613de3e902d650a4cf2e0e
Corey J. Nolet <cjnolet@gmail.com>
2014-05-04 12:41:29 -0400

    add license header to new example

--- src/main/java/org/calrissian/flowbox/example/AggregatorExample.java
+++ src/main/java/org/calrissian/flowbox/example/AggregatorExample.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright (C) 2014 The Calrissian Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.calrissian.flowbox.example;
 
 import org.calrissian.flowbox.example.support.ExampleRunner;
 import org.calrissian.flowbox.example.support.FlowProvider;
-import org.calrissian.flowbox.model.Event;
 import org.calrissian.flowbox.model.Flow;
 import org.calrissian.flowbox.model.Policy;
 import org.calrissian.flowbox.model.builder.FlowBuilder;
-import org.calrissian.flowbox.support.Criteria;
 import org.calrissian.flowbox.support.aggregator.CountAggregator;
 
 import java.util.List;

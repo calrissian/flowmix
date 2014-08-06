6d6e77491017e07b52dcc4f705f1731ba0f202ca
Corey J. Nolet <cjnolet@gmail.com>
2014-08-05 21:17:38 -0400

    Updating storm to 0.9.2

--- src/test/java/org/calrissian/flowmix/bolt/FlowTestCase.java
+++ src/test/java/org/calrissian/flowmix/bolt/FlowTestCase.java
@@ -15,14 +15,14 @@
  */
 package org.calrissian.flowmix.bolt;
 
+import java.io.Serializable;
+
 import backtype.storm.generated.StormTopology;
 import org.calrissian.flowmix.FlowmixFactory;
 import org.calrissian.flowmix.model.Flow;
 import org.calrissian.flowmix.spout.MockEventGeneratorSpout;
 import org.calrissian.flowmix.spout.MockFlowLoaderSpout;
 import org.junit.Before;
-
-import java.io.Serializable;
 
 import static java.util.Collections.singletonList;
 
@@ -31,6 +31,7 @@ public class FlowTestCase implements Serializable {
   @Before
   public void setUp() {
     MockSinkBolt.clear();
+    System.setProperty("java.net.preferIPv4Stack", "true");
   }
 
   protected StormTopology buildTopology(Flow flow, int intervalBetweenEvents) {
